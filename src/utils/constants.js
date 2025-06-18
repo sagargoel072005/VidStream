7cb738c69d393e80b45c893a72cc6eab16efd9e7
sagargoel072005 <goels4280@gmail.com>
2025-06-18 10:10:43 +0530

    optimization

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,6 @@
+export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
-export const GOOGLE_API_KEY = "AIzaSyCHG2rZxuZ0GfeVqtVXp6i5FVOi4lpHDD8";
+export const YOU_TUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=40&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
-export const YOU_TUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=40&regionCode=IN&key=" + GOOGLE_API_KEY ;
+export const YOU_TUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const YOU_TUBE_SEARCH_API =   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-
-/*
-export const GOOGLE_API_KEY=process.env.REACT_APP_GOOGLE_API_KEY;
-
-export const YOU_TUBE_API=process.env.REACT_APP_YOU_TUBE_API + GOOGLE_API_KEY ;
-
-export const YOU_TUBE_SEARCH_API=process.env.REACT_APP_YOU_TUBE_SEARCH_API;
-
-//IN&key=AIzaSyCHG2rZxuZ0GfeVqtVXp6i5FVOi4lpHDD8
-*/
