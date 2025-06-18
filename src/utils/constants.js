928bdd1dc3b4981b0d231f520402d9cc7ac6fb22
sagargoel072005 <goels4280@gmail.com>
2025-06-18 11:53:19 +0530

    api issues trial

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+// export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
+export const GOOGLE_API_KEY="AIzaSyCHG2rZxuZ0GfeVqtVXp6i5FVOi4lpHDD8";
 
 export const YOU_TUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=40&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
